e0768b632f89fd935d9a2901ebafa21df5ba512e
yogesh sharma <yogeshsharmma2003kh@gmail.com>
2025-12-21 00:16:17 +0530

    all done url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://chatappbackend-3-jz0f.onrender.com", {
   autoConnect: false, // important
 });
 
